fa842f4a86d8b36e0e011345c3cd8953571a1267
rkjar <rkjar@yandex.ru>
2022-04-29 22:04:10 +0500

    GET, POST editing

--- server/npv_app/app.py
+++ server/npv_app/app.py
@@ -4,11 +4,6 @@ from . import schemas, services
 app = FastAPI()
 
 
-@app.get('/')
-def root():
-    return {"message": "hello"}
-
-
-@app.post('/npv')
-def npv(npv_schema: schemas.NPVSchema):
+@app.post('/')
+def root(npv_schema: schemas.NPVSchema):
     return services.npv_counter(npv_schema)
